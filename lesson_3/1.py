c65697c6dab7113e78ecc0ad9e0cc36d6a4cde4c
Igor <pulupigor@ukr.net>
2019-11-08 12:44:04 +0200

    test commit

---
+++ lesson_3/1.py
@@ -0,0 +1,9 @@
+import random
+
+first_num=random.uniform(0,100)
+second_num=random.uniform(0,100)
+result=first_num*second_num
+print("Result is:")
+print(result)
+print('%.4f' % result)
+print('%.2f' % result)
