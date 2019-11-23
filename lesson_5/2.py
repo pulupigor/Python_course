ca36a963df247f156d8c8e3e48f5731e45d7f678
Igor <pulupigor@ukr.net>
2019-11-23 23:17:37 +0200

    Edit method of summ lists. Exc 2

--- lesson_5/2.py
+++ lesson_5/2.py
@@ -2,5 +2,5 @@ import string
 
 step=int(input("Please enter step for Cesar code: "))
 lowercase_leters=list(string.ascii_lowercase)
-cesar=dict(zip(lowercase_leters,lowercase_leters[step:].__add__(lowercase_leters[:step])))
+cesar=dict(zip(lowercase_leters,lowercase_leters[step:]+lowercase_leters[:step]))
 print("Cesar code with step %i is:" % step, cesar)
