71ae0ca4e010d4c6bdbafe54b81e0cbce8be078d
Igor <pulupigor@ukr.net>
2019-12-17 11:37:10 +0200

    Цикли

--- lesson_8/4.py
+++ lesson_8/4.py
@@ -2,7 +2,6 @@ with open ("zen_python") as zens_file:
     words=[]
     for lines in zens_file:
         words+=lines.strip().split(' ')
-    print(words)
     words=[i.strip('*!.-,').lower() for i in words if i.strip('*!.-,') != '']
     words=sorted(words,key=len)
     print(words)
